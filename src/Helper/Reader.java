125a405a9beed3e97e4a195cefa203f157ab3bc4
Deniz <cevherdz@ims.uni-stuttgart.de>
2018-04-11 17:38:34 +0200

    helper classes created. Reader and Writer

---
+++ src/Helper/Reader.java
@@ -0,0 +1,16 @@
+package Helper;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/**
+ * Created by deniz on 11.04.18.
+ */
+public class Reader extends BufferedReader{
+    public Reader(String fileName) throws FileNotFoundException {
+        super(new FileReader(fileName));
+    }
+}
+
+
---
+++ src/Helper/Writer.java
@@ -0,0 +1,15 @@
+package Helper;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by deniz on 11.04.18.
+ */
+public class Writer extends PrintWriter {
+    public Writer(String outFileName) throws IOException {
+        super(new BufferedWriter(new FileWriter(outFileName)));
+    }
+}
