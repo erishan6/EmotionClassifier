3e14f5cbcc2d1dd636a9e006fe0ddb8dc699ec52
Ishan Adhaulia <erishan6@gmail.com>
2018-04-18 17:20:29 +0200

    reading two files

--- src/com/teamlab/ss18/ec/Corpus.java
+++ src/com/teamlab/ss18/ec/Corpus.java
@@ -3,7 +3,6 @@ package com.teamlab.ss18.ec;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.UUID;
 
@@ -14,34 +13,38 @@ public class Corpus {
     private String name = "";
     private LinkedHashMap<UUID, Tweet> tweets;
     private String filePath = "";
-    public Corpus(String name, String filePath) {
+
+    public Corpus(String name, String filePath, String goldPath) {
         this.name = name;
         this.filePath = filePath;
 
-        tweets = createCorpus(this.filePath);
+        tweets = createCorpus(this.filePath, goldPath);
     }
 
 
-    private LinkedHashMap createCorpus(String filePath) {
+    private LinkedHashMap createCorpus(String filePath, String goldPath) {
         //TODO: read file into hashMap (like in util)
         LinkedHashMap tweetMap = null;
         try {
             tweetMap = new LinkedHashMap();
             Scanner scanner = new Scanner(new File(filePath));
             scanner.useDelimiter("\n");
-            while(scanner.hasNext()){
+            Scanner scanner2 = new Scanner(new File(goldPath));
+            scanner2.useDelimiter("\n");
+            while (scanner.hasNext() && scanner2.hasNext()) {
                 String mystring = scanner.next();
                 String arr[] = mystring.split("\t| ", 2);
                 String label = arr[0];
                 String sentence = arr[1];
-
-                Tweet currentTweet = new Tweet(sentence,label);
-
+                String goldLabel = scanner2.next();
+                Tweet currentTweet = new Tweet(sentence, goldLabel);
+                currentTweet.setPredictedLabel(label);
                 tweetMap.put(currentTweet.getId(), currentTweet);
 
             }
             scanner.close();
-
+            scanner2.close();
+            System.out.println(tweetMap.size());
         }
         catch (FileNotFoundException filenotfound) {
             filenotfound.printStackTrace();
