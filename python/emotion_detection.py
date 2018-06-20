4abb2cfef619518e2db9507e8b954c245a80f8d6
Deniz <cevherdz@ims.uni-stuttgart.de>
2018-06-20 14:46:48 +0200

    label to onehot added

--- python/emotion_detection.py
+++ python/emotion_detection.py
@@ -9,16 +9,38 @@ def load_data(filename, train_ratio = 0.7): #TODO: DENIZ
     :return:
     '''
 
+    def label_to_one_hot(array):
+        y_one_hot = []
+        for label in array:
+            one_hot_label = []
+            if (label == "anger"):
+                one_hot_label = [1, 0, 0, 0, 0, 0]
+            elif (label == "disgust"):
+                one_hot_label = [0, 1, 0, 0, 0, 0]
+            elif (label == "fear"):
+                one_hot_label = [0, 0, 1, 0, 0, 0]
+            elif (label == "joy"):
+                one_hot_label = [0, 0, 0, 1, 0, 0]
+            elif (label == "sad"):
+                one_hot_label = [0, 0, 0, 0, 1, 0]
+            elif (one_hot_label == "surprise"):
+                one_hot_label = [0, 0, 0, 0, 0, 1]
+            else:
+                one_hot_label = [0,0,0,0,0,0]
+            y_one_hot.append(np.array(one_hot_label))
+
+        return np.array(y_one_hot)
+
     data = np.loadtxt(filename, delimiter="\t", dtype=str, comments=None)
 
     number_training_instances = int(train_ratio * data.shape[0])
 
     data_train = data[0:number_training_instances]
-    y_train = data_train[:,0]
+    y_train = label_to_one_hot(data_train[:,0])
     x_train = data_train[:,1]
 
     data_test = data[number_training_instances:]
-    y_test = data_test[:,0]
+    y_test = label_to_one_hot(data_test[:,0])
     x_test = data_test[:,1]
 
     return x_train, y_train, x_test, y_test
@@ -55,8 +77,11 @@ def RNN(filename): #TODO: DENIZ
 
 def evaluate(y_gold, y_pred): #TODO: DENIZ
 
-    gold_counts_dict = np.unique(y_gold, return_counts=True)
-    pred_counts_dict = np.unique(y_pred, return_counts=True)
+    gold_counts_arrs = np.unique(y_gold, return_counts=True)
+    pred_counts_arrs = np.unique(y_pred, return_counts=True)
+
+    gold_counts_dict = dict(zip(gold_counts_arrs[0], gold_counts_arrs[1]))
+    pred_counts_dict = dict(zip(pred_counts_arrs[0], pred_counts_arrs[1]))
 
     print(gold_counts_dict)
 
@@ -74,5 +99,6 @@ if __name__ == "__main__":
 
 
     y_gold = [1, 2, 2, 1, 1, 1, 3]
+    y_pred = [1, 2, 1, 1, 3, 3, 1]
 
     evaluate(y_gold, y_pred)
